d5afefffc545454224f6cc567bcc1fcf148bd39a
lucas <luccasanttoz@gmail.com>
2022-11-21 00:11:56 -0300

    Cria gerador de modelos

--- Modelos.py
+++ Modelos.py
@@ -1,75 +1,29 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.modules import Module
 
-
-class MinhaRede(nn.Module):
-    def __init__(self, input_features, p=0.5):
-        super(MinhaRede, self).__init__()
-
-        self.camada_entrada = nn.Linear(input_features, 100)
-        self.camada_oculta_1 = nn.Linear(100, 50)
-        self.camada_oculta_2 = nn.Linear(50, 10)
-        self.camada_saida = nn.Linear(10, 2)
+class GeradorRede(nn.Module):
+    def __init__(self, input_size, layers_data: list, dropouts: list, p=0.5):
+        super(GeradorRede, self).__init__()
         
-        self.dropout_1 = nn.Dropout(p) # <= criação da camada de dropout, com cada neurônio tendo probabilidade p de ser desativado
-        self.dropout_2 = nn.Dropout(p) # <= criação da camada de dropout, com cada neurônio tendo probabilidade p de ser desativado
+        self.layers = nn.ModuleList()
+        self.input_size = input_size
+        self.p = p
 
-    def forward(self, p):
-        s = F.relu(self.camada_entrada(p))
-        s = self.dropout_1(s) # <= aplicamos a camada de dropout, que só faz efeito quando o modelo está em modo de treinamento
-        s = F.relu(self.camada_oculta_1(s))
-        s = self.dropout_2(s) # <= aplicamos a camada de dropout, que só faz efeito quando o modelo está em modo de treinamento
-        s = F.relu(self.camada_oculta_2(s))
-        s = self.camada_saida(s)
+        for size, activation in layers_data:
+            self.layers.append(nn.Linear(input_size, size))
+            input_size = size
+            if activation is not None:
+                assert isinstance(activation, Module), \
+                    "Each tuples should contain a size (int) and a torch.nn.modules.Module."
+                self.layers.append(activation)
 
-        return s
+        for dropout in dropouts:
+            self.layers.append(dropout)
+            
 
-class MinhaRedeV1(nn.Module):
-    def __init__(self, input_features, hidden_size, out_size, p=0.5):
-        super(MinhaNovaRede, self).__init__()
+    def forward(self, input_data):
+        for layer in self.layers:
+            input_data = layer(input_data)
+        return input_data
 
-        self.camada_entrada = nn.Linear(input_features, 100)
-        self.camada_oculta_1 = nn.Linear(100, 50)
-        self.camada_oculta_2 = nn.Linear(50, 10)
-        self.camada_saida = nn.Linear(10, 2)
-
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=-1)
-        
-        self.dropout_1 = nn.Dropout(p) # <= criação da camada de dropout, com cada neurônio tendo probabilidade p de ser desativado
-        self.dropout_2 = nn.Dropout(p) # <= criação da camada de dropout, com cada neurônio tendo probabilidade p de ser desativado
-
-    def forward(self, p):
-        s = self.relu(self.camada_entrada(p))
-        s = self.dropout_1(s) # <= aplicamos a camada de dropout, que só faz efeito quando o modelo está em modo de treinamento
-        s = self.relu(self.camada_oculta_1(s))
-        s = self.dropout_2(s) # <= aplicamos a camada de dropout, que só faz efeito quando o modelo está em modo de treinamento
-        s = F.relu(self.camada_oculta_2(s))
-        s = self.softmax(self.camada_saida(s))
-
-        return s
-
-class MinhaNovaRede(nn.Module):
-    def __init__(self, input_features, p=0.5):
-        super(MinhaNovaRede, self).__init__()
-
-        self.camada_entrada  = nn.Linear(input_features, 100)
-        self.camada_oculta_1 = nn.Linear(100, 50)
-        self.camada_oculta_2 = nn.Linear(50, 10)
-        self.camada_saida    = nn.Linear(10, 2)
-
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=-1)
-        
-        self.dropout_1 = nn.Dropout(p) # <= criação da camada de dropout, com cada neurônio tendo probabilidade p de ser desativado
-        self.dropout_2 = nn.Dropout(p) # <= criação da camada de dropout, com cada neurônio tendo probabilidade p de ser desativado
-
-    def forward(self, p):
-        s = self.relu(self.camada_entrada(p))
-        s = self.dropout_1(s) # <= aplicamos a camada de dropout, que só faz efeito quando o modelo está em modo de treinamento
-        s = self.relu(self.camada_oculta_1(s))
-        s = self.dropout_2(s) # <= aplicamos a camada de dropout, que só faz efeito quando o modelo está em modo de treinamento
-        s = F.relu(self.camada_oculta_2(s))
-        s = self.softmax(self.camada_saida(s))
-
-        return s
