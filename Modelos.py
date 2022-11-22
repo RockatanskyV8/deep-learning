e4cabea913d874ef7e122d2c44ea0aa73deb8386
lucas <luccasanttoz@gmail.com>
2022-11-21 21:56:20 -0300

    Cria classe para criação de camadas e incorpora com o gerador de modelos

--- Modelos.py
+++ Modelos.py
@@ -2,25 +2,30 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules import Module
 
+class Camada():
+    def __init__(self, size, activation, dropout = None):
+        self.size = size
+        self.activation = activation
+        self.dropout = dropout
+
 class GeradorRede(nn.Module):
-    def __init__(self, input_size, layers_data: list, dropouts: list, p=0.5):
+    def __init__(self, input_size, layers_data: list):
         super(GeradorRede, self).__init__()
         
         self.layers = nn.ModuleList()
         self.input_size = input_size
-        self.p = p
 
-        for size, activation in layers_data:
-            self.layers.append(nn.Linear(input_size, size))
-            input_size = size
-            if activation is not None:
-                assert isinstance(activation, Module), \
+        for layer in layers_data:
+            self.layers.append(nn.Linear(input_size, layer.size))
+            input_size = layer.size
+
+            if layer.activation is not None:
+                assert isinstance(layer.activation, Module), \
                     "Each tuples should contain a size (int) and a torch.nn.modules.Module."
-                self.layers.append(activation)
+                self.layers.append(layer.activation)
 
-        for dropout in dropouts:
-            self.layers.append(dropout)
-            
+            if layer.dropout is not None:
+                self.layers.append( layer.dropout )
 
     def forward(self, input_data):
         for layer in self.layers:
--- teste.py
+++ teste.py
@@ -14,7 +14,6 @@ from Modelos import *
 from Reader import *
 from Treinamento import Treinamento
 ################################################################################################
-input_features = 3
 df = pd.read_csv('../df_points.txt', sep='\t', index_col=[0])
 
 r = Reader(df)
@@ -23,7 +22,23 @@ train, valid = r.read()
 X_train, X_test, y_train, y_test = train
 X_valid, X_test, y_valid, y_test = valid
 
-model = MinhaNovaRede(input_features)
+softmax = nn.Softmax(dim=-1)
+
+input_features = 3
+output_size = 2
+p = 0.5
+
+camada1 = Camada(500, nn.ReLU(), nn.Dropout(p) )
+camada2 = Camada(500, nn.ReLU(), nn.Dropout(p) )
+camada3 = Camada(2, softmax)
+
+model = GeradorRede( input_features , [Camada(500, nn.ReLU(), nn.Dropout(p) ),
+                                       Camada(250, nn.ReLU(), nn.Dropout(p) ),
+                                       Camada(100, nn.ReLU(), nn.Dropout(p) ),
+                                       Camada( 10, nn.ReLU(), nn.Dropout(p) ),
+                                       Camada(  2, softmax ),
+                                       ])
+#  model = MinhaNovaRede(input_features)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
@@ -42,6 +57,16 @@ model, train_loss, valid_loss = r.classificacao(optimizer, criterion,
 
 p = Plots()
 accuracy_final = p.get_accuracy(model, torch.from_numpy(X_test), torch.from_numpy(y_test.to_numpy()))
-
 print(accuracy_final)
+
+#  (camada_entrada): Linear(in_features=3, out_features=500, bias=True)
+#  (camada_oculta_1): Linear(in_features=500, out_features=250, bias=True)
+#  (camada_oculta_2): Linear(in_features=250, out_features=100, bias=True)
+#  (camada_oculta_3): Linear(in_features=100, out_features=10, bias=True)
+#  (camada_saida): Linear(in_features=10, out_features=2, bias=True)
+#  (relu): ReLU()
+#  (softmax): Softmax(dim=-1)
+#  (dropout_1): Dropout(p=0.125, inplace=False)
+#  (dropout_2): Dropout(p=0.25, inplace=False)
+#  (dropout_3): Dropout(p=0.375, inplace=False)
 
